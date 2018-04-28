a59827cd18c5b8eba809ff1f394fe9ebc8ff3dd7
Patrick Watson <patrick@patrickwatson.org>
2018-04-28 01:48:01 -0400

    Disable package_cloud deploy to Hypriot repo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,9 @@
 echo "Circle Tag: $CIRCLE_TAG"
 
 if [ "$CIRCLE_TAG" != "" ]; then
-  gem install package_cloud
-  package_cloud push Hypriot/rpi/debian/stretch output/*/raspberrypi-kernel*.deb output/*/linux-libc-dev*.deb
+  # comment out the push to a package manager until I get an account
+  #gem install package_cloud
+  #package_cloud push Hypriot/rpi/debian/stretch output/*/raspberrypi-kernel*.deb output/*/linux-libc-dev*.deb
 
   curl -sSL https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_linux_amd64.zip -o ghr.zip
   unzip ghr.zip
